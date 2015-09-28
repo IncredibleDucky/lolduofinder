9fd433c31a5ea1d49e07cd7aadca7b48a55fe95d
Matt Bailey <incredibleducky@gmail.com>
2015-09-28 04:55:10 -0700

    sorting logic/ matches/messaging

---
+++ testing swiping/SummonerTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SummonerTableViewCell.h
+//  testing swiping
+//
+//  Created by Mattthew Bailey on 9/27/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummonerTableViewCell : UITableViewCell
+
+@end
---
+++ testing swiping/SummonerTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  SummonerTableViewCell.m
+//  testing swiping
+//
+//  Created by Mattthew Bailey on 9/27/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import "SummonerTableViewCell.h"
+
+@implementation SummonerTableViewCell
+
+@end
